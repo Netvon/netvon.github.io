b8715db4c9aa4a47c4a8dd25fb67e1b3e527082f
Netvon <tomvannimwegen@gmail.com>
2014-10-28 00:54:30 +0100

    html, js, css update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
-// jQuery(
-// 	function ($) {
-// 		$(".navbutton").click(function(event) {
-// 			alert("Hallo");
-// 		});
-// 	}
+$( document ).ready(function() {
+	$('.nav-element:first-child').click(function(event) {
+		invert();
+	});
 
-// 	function convertMenu()
-// 	{
-// 		$(".navigation ul").children().
-// 	}
-// )
+	$(window).resize(function(event) {
+		var width = $(window).width();
+		var visible = $('.nav-link').is(':visible');
 
+		if(width >= 480 &&
+			!visible)
+		{
+			invert();
+			return;
+		}
+
+		if(width < 480 &&
+			visible)
+		{
+			invert();
+			return;
+		}
+	});
+	
+	function invert()
+	{
+		var logo = $('.nav-element:first-child');
+		var navlinks = $('.nav-link');
+		if(navlinks.is(':visible'))
+		{
+			navlinks.css('display', 'none');
+			logo.attr('data-content', 'Show menu ↓');
+		}else
+		{
+			navlinks.css('display', 'inherit');
+			logo.attr('data-content', 'Hide menu ↑');
+		}
+	}
+});
